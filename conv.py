3ce7f545b5ca4e78e2863d9d1f05d0d9ac20e9ed
v3arms <kirilchenko.aa@phystech.edu>
2020-07-14 19:32:02 +0300

    init

---
+++ conv.py
@@ -0,0 +1,93 @@
+from openpyxl import load_workbook
+import scipy as sp
+import scipy.sparse as scsp
+import numpy as np
+from scipy.io import savemat
+import json
+from scipy.io import loadmat
+import networkx as nx
+import sys
+from os.path import splitext
+import os
+
+
+def f7(seq):
+    seen = set()
+    seen_add = seen.add
+    return [x for x in seq if not (x in seen or seen_add(x))]
+
+
+if len(sys.argv) < 2 :
+    print('Specify .xlsx')
+    sys.exit(0)
+data = load_workbook(filename=sys.argv[1], read_only=True)
+
+
+id2node    = {}
+node2id    = {}
+i = []
+j = []
+v = []
+
+
+cur_node = 0
+datas = []
+for rown, vals in enumerate(data.active.values) :
+    if (rown == 0) :
+        continue
+    if (rown % 50000 == 0) :
+        print(rown, " processed")
+
+    if vals[0] not in id2node :
+        id2node[vals[0]] = cur_node
+        node2id[cur_node] = vals[0]
+        cur_node = cur_node + 1
+    
+    if vals[2] not in id2node :
+        id2node[vals[2]] = cur_node
+        node2id[cur_node] = vals[2]
+        cur_node = cur_node + 1
+    
+    datas.append(str(id2node[vals[0]]) + " " + str(id2node[vals[2]]))
+    datas.append(str(id2node[vals[2]]) + " " + str(id2node[vals[0]]))
+
+    i.append(id2node[vals[0]])
+    j.append(id2node[vals[2]])
+    i.append(id2node[vals[2]])
+    j.append(id2node[vals[0]])
+    v.append(1)
+    v.append(1)
+
+
+A = scsp.coo_matrix((v, (i, j))).tocsr()
+A.sum_duplicates()
+A.data[:] = 1
+
+
+output_dir  = splitext(sys.argv[1])[0]
+
+try :
+    os.mkdir(output_dir)
+except OSError :
+    pass
+
+output_file = splitext(sys.argv[1])[0] + ".edgelist"
+G = nx.from_scipy_sparse_matrix(A, create_using=nx.DiGraph)
+nx.write_edgelist(G, output_dir + '/' + output_file, data=False)
+
+
+f = open(output_dir + '/' + output_file, 'r')
+s = f.read()
+s = str(cur_node) + '\n' + s
+f.close()
+f = open(output_dir + '/' + output_file, 'w')
+f.write(s)
+
+f.flush()
+f.close()
+
+print("dump mappings..")
+with open(output_dir + '/node2id.json', 'w') as f :
+    json.dump(node2id, f)
+with open(output_dir + '/id2node.json', 'w') as f :
+    json.dump(id2node, f)
---
+++ main.cpp
@@ -0,0 +1,105 @@
+/************************************************* 
+Copyright:
+Author: Yu Liu, Xiaodong He, Zhewei Wei
+Date: 2017-12-25 
+Libraries Required: 
+	SFMT-1.4.1: A random number generator, which can be downloaded here:
+	www.math.sci.hiroshima-u.ac.jp/~m-mat/MT/SFMT
+**************************************************/
+
+#include "SimStruct.h"
+#include "util.h"
+#include "json.hpp"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+using json = nlohmann::json;
+
+void usage() {
+	cerr << "./ssrank [data_dir] [query_file] [-o <output dir, default: 'data_dir_name_output'>] [ -e<eps, default: 0.005>] " << endl;
+	cerr << "data_dir format :\ndata_dir/\n\tgraph.edgelist\n\tid2node.json\n\tnode2id.json\n";
+	cerr << "Example : ./ssrank nppro query/0.query -q query/0.query" << endl;
+	exit(EXIT_SUCCESS);
+}
+
+
+int main(int argc, char** argv){
+	string file = "";
+	string qf = "";
+	vector<string> queryNode;
+	double eps = 0.005;
+
+	if (argc < 3) 
+		usage();
+
+	string data_dir(argv[1]), queryfile(argv[2]), output_dir(data_dir + "_output");
+
+	for (int i = 3; i < argc; i++) {
+		if (!strncmp(argv[i], "-e", 2))
+			eps = strtod(argv[++i], NULL);
+		else if (!strncmp(argv[i], "-o", 2))
+			output_dir = string(argv[++i]);
+		else
+			usage();
+	}
+
+	queryNode = readfile(queryfile);
+	std::ifstream node2idStream;
+	std::ifstream id2nodeStream;
+	struct stat st = {0}; 
+	errno = 0;
+	node2idStream.open(data_dir + "/node2id.json", ios_base::in);
+	id2nodeStream.open(data_dir + "/id2node.json", ios_base::in);
+	if (errno != 0) {
+		cerr << strerror(errno) << endl;
+		return 0;
+	}
+	if (stat(output_dir.c_str(), &st) == -1)
+    	mkdir(output_dir.c_str(), 0700);
+
+	json node2id; 
+	json id2node; 
+	if (!((node2idStream >> node2id) && (id2nodeStream >> id2node))) {
+		std::cout << "json read error!\n";
+		return 0;
+	}
+	node2idStream.close();
+	id2nodeStream.close();
+	
+	queryNode.pop_back();
+
+	if (queryNode.empty()) {
+		std::cout << "No queries given in query file\n";
+		return 0;
+	}
+	SimStruct sim = SimStruct(data_dir + "/graph.edgelist", eps); 
+
+	for(int i = 0; i < queryNode.size(); i++){
+		if (queryNode[i] == "\n") break;	
+		try {
+			id2node.at(queryNode[i]);
+		} catch (...) {
+			// std::cout << e.what() << std::endl;
+			cout << "Mapping error, node " << queryNode[i] << endl;
+			cout << "Skipping this node..\n";
+			return 0;
+		}
+		cout << "node: " << queryNode[i] << " " << id2node[queryNode[i]] << "\t";
+		auto sims = sim.batch(id2node[queryNode[i]]);
+
+		stringstream ss_out;
+		ss_out << output_dir << "/" << "result" << i;
+
+		ofstream of_res(ss_out.str());
+		std::cout << ss_out.str() << " " << sims.size() << std::endl;
+		for(int j=0; j<sims.size(); j++)
+			of_res << queryNode[i] << "," << node2id[to_string(int(sims[j].first))].get<std::string>() << "," << sims[j].second << '\n';
+		
+		of_res.flush(); 
+		of_res.close();
+	}
+
+	return 0;
+}
